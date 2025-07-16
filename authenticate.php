7ad4cd8e8ed16f17d3ae6e9558d4ea1218db4444
SamirTaous <hgta626@gmail.com>
2025-07-16 12:57:12 +0100

    Configured Basic Registration

--- authenticate.php
+++ authenticate.php
@@ -1,23 +1,42 @@
 <?php
 
-$servername = "localhost";
-$username = "samirtaous";
-$password = "";
-$db = "php_chat";
+require_once("config.php");
 
-// Create Connection
-
-$conn = new mysqli($servername, $username, $password, $db);
-
-// Login Logic
-
-echo $_SERVER["REQUEST_METHOD"];
-
-if(isset($_POST['username'])){
+if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['submit'])){
     $submitted_username = $_POST['username'];
-    echo "Thank you for registering! Your submitted username was: " . htmlspecialchars($submitted_username);
+    $submitted_password = $_POST['password'];
+    $hashed_password = password_hash($submitted_password,PASSWORD_DEFAULT);
+
+    
+    $sql = "SELECT * FROM users WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt = bind_param("s",$submitted_username);
+    $show = $stmt->execute();
+    echo "$show"; 
 }
-else{
-    echo "Username doesn't exist in the Post request";
-}
+
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <h2>Login with your account</h2>
+        <form action="" method="post">
+            <label for="username"> Username :</label>
+            <input id="username" type="text" name="username"></input>
+            <br>
+            <label for="password"> Password :</label>
+            <input id="password" type="password" name="password"></input>
+            <br>
+            <input type="submit" value="Submit"></input>
+        </form>
+    </body>
+</html>
---
+++ register.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once("config.php");
+
+if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['username'])){
+    
+    // Get Raw data from the form
+    $submitted_username = $_POST['username'];
+    $submitted_password = $_POST['password'];
+    $hashed_password = password_hash($submitted_password, PASSWORD_DEFAULT);
+    
+    // Prepare the SQL Statement 
+
+    $sql = ("INSERT INTO users(username,password) VALUES(?,?);");
+    $stmt = $conn->prepare($sql);
+
+    // Bind the user parameters to the placeholders
+
+    $stmt->bind_param("ss",$submitted_username,$hashed_password);
+
+    // Execute the statement and check for success
+
+    if($stmt->execute()){
+        echo "User registered successfully, you can now log in";
+    }
+    else{
+        echo "User not registered, please try again";
+    }
+
+    // Closing the statement and connection
+
+    $stmt->close();
+    $conn->close();
+
+    header("location: authenticate.php");
+}
+
+?>
